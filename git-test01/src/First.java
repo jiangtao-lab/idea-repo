742dd438bc554a0bb44f598f2a292736200e8748
jiangtao <1551387171@qq.com>
2021-11-09 21:54:16 +0800

    从远端克隆下来之后，在IDEA中修改了该项目

--- git-test01/src/First.java
+++ git-test01/src/First.java
@@ -6,9 +6,11 @@ public class First {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+//        克隆下来之后，进行修改
     }
 
     public void testMethod(){
         System.out.println("test method...");
+        System.out.println("克隆下来之后修改了testMethod方法！！");
     }
 }
